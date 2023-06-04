4a6439a25eab3de5724c63d93f4cafbc960869db
vikram <vikram.hamal.official@gmail.com>
2023-06-04 14:33:09 +0530

    this is nature base website

---
+++ index.js
@@ -0,0 +1,5 @@
+let header = document.getElementById('header')
+let mobile_menu = document.getElementById('mobile-menu')
+mobile_menu.addEventListener('click',()=>{
+    header.classList.toggle('show-class')
+})
